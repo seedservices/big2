8bd4516fd3c06a20164bcc31b1398490ddfa5a96
SEED Services <seedservices32@gmail.com>
2026-03-20 23:35:19 +0000

    Adjust mobile hand overlap sizing

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-﻿const RANKS=['3','4','5','6','7','8','9','10','J','Q','K','A','2'];
+﻿﻿const RANKS=['3','4','5','6','7','8','9','10','J','Q','K','A','2'];
 const SUITS=[
   {symbol:'♦️',red:true},
   {symbol:'♣️',red:false},
