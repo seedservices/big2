0ec4714ff1abf38a58ac4c9706f4da69cf1b36a1
seedservices <seedservices32@gmail.com>
2026-03-24 22:07:37 +0000

    Change base path in Vite config to '/big2/'

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 ﻿import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/big2/',
   define: {
     'import.meta.env.ENV': JSON.stringify(process.env.ENV ?? '')
   }
